ac0c08df9dee23e821248ee8e19c7e7688435a40
Laci Cartmell <57330789+laci-cartmell@users.noreply.github.com>
2022-12-07 16:50:08 -0600

    Problem 3, hw 4

--- 4scriptv2.R
+++ 4scriptv2.R
@@ -10,34 +10,28 @@ library(tidyverse)
 MBT_ebird <- read.csv("https://github.com/mbtoomey/Biol_7263/blob/main/Data/MBT_ebird.csv?raw=true") #read in my ebird csv file from github account
 
 View(MBT_ebird)   #View the file
+#count_tot -> total number of birds seen that day
+#count -> count of times a species was observed
 
-## Problem 2
+
+## Problem 1
 ## Which year has the most individual birds, how many?
 
 # group by year, the summarize count for each year?
-<<<<<<< HEAD
-
-MBT_COUNT2 <- MBT_ebird %>%
-  group_by(year) %>%
-  summarise(year)
-
-View(MBT_COUNT2)
 
 
 MBT_COUNT <- MBT_ebird %>%
   group_by(year) %>%
   mutate(year_count = n())
-=======
+
 # Now, group by year, and create col of year counts
 MBT_COUNT <- MBT_ebird %>%
   group_by(year) %>%
   mutate(year_count = n())
 #Check year count worked
->>>>>>> 29582be94020f2340208a29ae3dd90329e4a4741
 View(MBT_COUNT)
 #    good2go
 
-<<<<<<< HEAD
 val <- max(MBT_COUNT$year_count)
 print(paste("The max count is", val))
 
@@ -45,38 +39,48 @@ years <- filter(MBT_COUNT, year_count == 1672)
 yearmaxval <- max(years$year)
 print(paste(yearmaxval, "had the max count of birds"))
   
-## Problem 2
-#in that year, how many different species did I obser\
-years %>%
-  group_by(scientific_name) %>%
-  mutate(year_count = n())
+View(years)
 
-
-d
-=======
-#output max value w/in year count
-maxcount <- max(MBT_COUNT$year_count)
-#tibble with only values from the year with most counts
-maxyear <- filter(MBT_COUNT, year_count == val)
-View(maxyear)
- 
+############
 ## Problem 2
 ## in that year, how many different species did I observe?
-#count all unique instance of name
-species_count <- n_distinct(maxyear$scientific_name)
-species_count
 
+
+#count all unique instance of name
+species_count <- n_distinct(years$scientific_name)
+species_count
+print(paste(species_count, "species were observed."))
+
+############
 ## Problem 3
 ## Which state, most freq. observe red-winged blackbirds
-#use search to find pattern of red-winged blackbirds
+#use filter to find pattern of red-winged blackbirds
 #create new tibble with only those matches
 #group by state, create count var
-#print resutlsyear has the most individual birds, how many?
+
+RWB_COUNT <- MBT_ebird %>%
+  filter(common_name == "Red-winged Blackbird") %>%
+  group_by(location) %>%
+  mutate(location_count = n())
+View(RWB_COUNT)
+
+# Location_count * count to give total # of rwb in each state
+RWB_COUNT <- mutate(RWB_COUNT, stateobs = location_count * count)
+View(RWB_COUNT)
+
+#max ohs. from RWB Counts
+toprwb <- max(RWB_COUNT$stateobs)
+
+statetoprwb <- filter(RWB_COUNT, stateobs == toprwb)
+View(statetoprwb)
+
+print(paste(statetoprwb$location, "is where most RWBs were found in "))
 
 
 ## Problem 4
 ## Filter 5-200min duration. Find mean rate per checklist that I encounter species each year
 # calculate number of species in each checklist / duration - take mean for year
+
 
 #use search to find pattern of red-winged blackbirds
 #create new tibble with only those matches
