0fc08544af33486302f391b318bee8a58e1561b5
Laci Cartmell <57330789+laci-cartmell@users.noreply.github.com>
2022-12-14 22:51:22 -0600

    website links

--- Assignments/GraduateLessons/Madison/Heatmaps_Madison.R
+++ Assignments/GraduateLessons/Madison/Heatmaps_Madison.R
@@ -38,28 +38,7 @@ heatmap(heatmap_num)
 
 # Make it pretty with ggplot
 
-ggplot(mah, aes(x = Sample, y = Gene, fill = Expression)) +
-  geom_tile(colour="black", linewidth=0.5)+ 
-  scale_fill_gradient(low = "black", high = "yellow")+
-  theme_grey(base_size=12)+
-  facet_grid(~ Tissue, switch = "x", scales = "free_x", space = "free_x") +
-  ggtitle(label = "Pheromone Gene Expression") +
-  scale_x_discrete(labels=c('A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez', 
-                            'A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez',
-                            'A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez',
-                            'A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez'))+
-  theme(plot.title = element_text(face="bold"),
-        strip.placement = "outside",
-        strip.text = element_text(face = "bold"),
-        legend.title = element_text(face = "bold"),
-        axis.title = element_text(face="bold"),
-        axis.title.x = element_blank(),
-        axis.text.y =element_text(color = "black"),
-        axis.text.x =element_text(angle = 315, hjust = 0, vjust = 0.5, color = "black"),
-        axis.ticks=element_blank(),
-        plot.background=element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_blank())
+
 
 # create heatmap with 
 ggplot(heatmap_num, aes(x = Sample, y = Gene, fill = Expression)) +
@@ -70,15 +49,13 @@ ggplot(heatmap_num, aes(x = Sample, y = Gene, fill = Expression)) +
   facet_grid(~ Tissue, switch = "x", scales = "free_x", space = "free_x")
   ggtitle(label = "Pheromone Gene Expression by Tissue Type") +
   scale_x_discrete(labels=c('A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez'))+
-    #now set up the themes
-  theme(plot.title = element_text(face="bold"),
+    theme(plot.title = element_text(face="bold"),
         strip.placement = "outside", 
         legend.title = element_text(face = "bold"),
         axis.title = element_text(face="bold"),
         axis.title.x = element_blank(),
         axis.text.y =element_text(color = "black"),
-        axis.text.x =element_text(color = "black"),
-        axis.text.x =element_text(angle = 315, hjust = 0, vjust = 0.5, color = "black"),
+        axis.text.x =element_text(color = "black", angle = 315, hjust = 0, vjust = 0.5,),
         axis.ticks=element_blank(),
         plot.background=element_blank(),
         panel.background = element_blank(),
