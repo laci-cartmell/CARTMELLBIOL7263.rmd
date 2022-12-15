91ec5933c62d203656f015787aeda97a94cdface
Laci Cartmell <57330789+laci-cartmell@users.noreply.github.com>
2022-12-14 22:32:57 -0600

    Heatmap assing

--- Assignments/GraduateLessons/Madison/Heatmaps_Madison.R
+++ Assignments/GraduateLessons/Madison/Heatmaps_Madison.R
@@ -1,8 +1,8 @@
 ##  _____________________
 ##
-## Script: Heatmaps in R
+## Script: Learn how to do Heatmaps in R
 ##
-## Purpose of script:Lesson from maddison
+## Purpose of script:Lesson from Madison H. 
 ##
 ## Author: Laci Cartmell
 ##
@@ -21,103 +21,64 @@
 library(stats)
 library(ggplot2)
 
+## Load in the dataset
 
-pheromone_data <- read.csv("pheromone_data.csv")
-#in order to use the heatmap() function, we need to transform the csv file into a numeric data frame
-#this also allows us to make sure we keep our rownames
-pheromone_data1 <- as.matrix(pheromone_data[,-1])
-rownames(pheromone_data1) <- pheromone_data[,1]
-heatmap(pheromone_data1)
+heatmap_practice <- read_csv("MAH_assignment_data.csv")
+View(heatmap_practice)
 
-heatmap(pheromone_data1, 
-        Colv = NA, #get rid of column clustering
-        Rowv = NA, #get rid of row clustering
-        margins = c(7,5), #change the margins so we can fit in the column names with the axis title
-        cexCol = 1, #change the size of the column names
-        main = "Pheromone Gene Expression", #add a title
-        xlab = "Groups", #add an x axis label
-        ylab = "Gene") #add a y axis label
+## Create heatmap w/ggplot2 faceted by tissue type
 
-heatmap(pheromone_data1, 
-        Colv = NA, #get rid of column clustering
-        Rowv = NA, #get rid of row clustering
-        margins = c(7,5), #change the margins so we can fit in the column names with the axis title
-        cexCol = 1, #change the size of the column names
-        col = terrain.colors(10), #changing the color palette and selecting how many colors to use
-        main = "Pheromone Gene Expression", #add a title
-        xlab = "Groups", #add an x axis label
-        ylab = "Gene") #add a y axis label
+# transform data to numeric
+#remove rownames and saves them
+heatmap_num <- as.matrix(heatmap_practice[,-1])
+rownames(heatmap_num) <- heatmap_practice[,1]
 
+#view with basic functions
+heatmap(heatmap_num)
 
-## ggplot2
-pheromone_data2 <- read.csv("pheromone_data2.csv")
-ggplot(pheromone_data2, #give it the data
-       aes(x = Sample, y = Gene, fill = Expression)) + #set our x and y and what data we want to "fill" our heatmap with
-  geom_tile() #the heatmap plot function
+# Make it pretty with ggplot
 
-
-ggplot(pheromone_data2, aes(x = Sample, y = Gene, fill = Expression)) +
-  geom_tile(colour="black", size=0.5)+ #here we are adding outlines around our tiles
-  #we can change the colour and thickness of this outline
-  scale_fill_gradient(low = "black", high = "green")+ #change the color gradient
-  theme_grey(base_size=12)+ #change our theme and make our base size font 12
-  theme(axis.ticks = element_blank(), #get rid of the tick marks
-        #get rid of all the background ggplot2 aesthetics
-        plot.background = element_blank(), 
-        panel.background = element_blank(),
-        panel.border = element_blank())
-
-ggplot(pheromone_data2, aes(x = Sample, y = Gene, fill = Expression)) +
-  geom_tile(colour="black", size=0.5)+ 
-  scale_fill_gradient(low = "black", high = "green")+
+ggplot(mah, aes(x = Sample, y = Gene, fill = Expression)) +
+  geom_tile(colour="black", linewidth=0.5)+ 
+  scale_fill_gradient(low = "black", high = "yellow")+
   theme_grey(base_size=12)+
-  ggtitle(label = "Pheromone Gene Expression") + #add a plot title
-  theme(plot.title = element_text(face="bold"), #make the plot title bold
-        legend.title = element_text(face = "bold"), #make the legend title bold
-        axis.title = element_text(face="bold"), #make the axis titles bold
-        axis.text.y =element_text(color = "black"), #make the axis labels black instead of grey
-        axis.text.x =element_text(angle = 270, hjust = 0, color = "black"), #change the angle of the axis labels,
-        #the position, and the color
-        axis.ticks= element_blank(),
-        plot.background=element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_blank())
-
-ggplot(pheromone_data2, aes(x = Sample, y = Gene, fill = Expression)) +
-  geom_tile(colour="black", size=0.5)+ 
-  scale_fill_gradient(low = "black", high = "green")+
-  theme_grey(base_size=12)+
-  facet_grid(~ Gland) + #here we are faceting by gland
+  facet_grid(~ Tissue, switch = "x", scales = "free_x", space = "free_x") +
   ggtitle(label = "Pheromone Gene Expression") +
-  scale_x_discrete(labels=c('D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb'))+ #here we are changing the x axis labels
+  scale_x_discrete(labels=c('A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez', 
+                            'A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez',
+                            'A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez',
+                            'A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez'))+
   theme(plot.title = element_text(face="bold"),
+        strip.placement = "outside",
+        strip.text = element_text(face = "bold"),
         legend.title = element_text(face = "bold"),
         axis.title = element_text(face="bold"),
         axis.title.x = element_blank(),
         axis.text.y =element_text(color = "black"),
-        axis.text.x =element_text(color = "black"),
+        axis.text.x =element_text(angle = 315, hjust = 0, vjust = 0.5, color = "black"),
         axis.ticks=element_blank(),
         plot.background=element_blank(),
         panel.background = element_blank(),
         panel.border = element_blank())
 
-ggplot(pheromone_data2, aes(x = Sample, y = Gene, fill = Expression)) +
+# create heatmap with 
+ggplot(heatmap_num, aes(x = Sample, y = Gene, fill = Expression)) +
   geom_tile(colour="black", size=0.5)+ 
   scale_fill_gradient(low = "black", high = "green")+
   theme_grey(base_size=12)+
-  facet_grid(~ Gland, switch = "x", scales = "free_x", space = "free_x") + #switch moves the facet labels to the bottom
-  #applying "free_x" removes the columns for which there is no data and
-  #applying "free_x" to space makes the columns the same width
-  ggtitle(label = "Pheromone Gene Expression") +
-  scale_x_discrete(labels=c('D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb'))+
+  #now create the facets by tissue type
+  facet_grid(~ Tissue, switch = "x", scales = "free_x", space = "free_x")
+  ggtitle(label = "Pheromone Gene Expression by Tissue Type") +
+  scale_x_discrete(labels=c('A.tri', 'D.brim', 'E.tyn M', 'E.tyn P', 'P.alb', 'R.kez'))+
+    #now set up the themes
   theme(plot.title = element_text(face="bold"),
-        strip.placement = "outside", #this places the facet label outside the x axis labels
-        strip.text = element_text(face = "bold"), #this bolds the text in the facet label
+        strip.placement = "outside", 
         legend.title = element_text(face = "bold"),
         axis.title = element_text(face="bold"),
         axis.title.x = element_blank(),
         axis.text.y =element_text(color = "black"),
         axis.text.x =element_text(color = "black"),
+        axis.text.x =element_text(angle = 315, hjust = 0, vjust = 0.5, color = "black"),
         axis.ticks=element_blank(),
         plot.background=element_blank(),
         panel.background = element_blank(),
